67cc882aca9f077566ecb0e967017db4a379090c
Adrian Lara <adrianblara@gmail.com>
2017-10-16 15:07:51 -0600

    added pry, user can update job written as well as tested

---
+++ spec/features/jobs/user_can_edit_job_information_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "When a user visits the job edit page, changes the job information, and clicks 'Update Job'" do
+  it "the user has updated the job and sees its show page" do
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+
+    visit edit_company_job_path(company,job)
+
+    save_and_open_page
+
+    fill_in "job[title]", with: "Intern"
+    click_on "Update Job"
+
+    expect(current_path).to eq(company_job_path(company, job))
+    expect(page).to have_content("Intern")
+  end
+end
