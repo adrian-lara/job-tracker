bfb38f873220f47ebf24ae0177b93af160007162
Adrian Lara <adrianblara@gmail.com>
2017-10-16 16:45:01 -0600

    user can delete a job from job show page written and tested

--- spec/features/jobs/user_can_delete_a_job_spec.rb
+++ spec/features/jobs/user_can_delete_a_job_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "When a user clicks the link to delete a job" do
+describe "When a user clicks the link to delete a job in job index page" do
   it "the user has deleted that job and now seees the company show/ job index page" do
     company = Company.create!(name: "ESPN")
     company.jobs.create!(title: "Developer", level_of_interest: 90, city: "Denver")
@@ -8,6 +8,21 @@ describe "When a user clicks the link to delete a job" do
 
     visit company_jobs_path(company)
     click_link "Delete", href: company_job_path(company, company.jobs.first)
+
+    expect(page).not_to have_link("Developer")
+    expect(page).to have_link("Intern")
+    expect(company.jobs.count).to eq(1)
+  end
+end
+
+describe "When a user clicks the link to delete a job in job show page" do
+  it "the user has deleted that job and now seees the company show/ job index page" do
+    company = Company.create!(name: "ESPN")
+    company.jobs.create!(title: "Developer", level_of_interest: 90, city: "Denver")
+    company.jobs.create!(title: "Intern", level_of_interest: 99, city: "Denver")
+
+    visit company_job_path(company,company.jobs.first)
+    click_link "Delete"
 
     expect(page).not_to have_link("Developer")
     expect(page).to have_link("Intern")
