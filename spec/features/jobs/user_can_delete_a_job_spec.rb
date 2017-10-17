118cc4ed1dd7edf2cfd31fde7b2459b885e5679f
Adrian Lara <adrianblara@gmail.com>
2017-10-17 15:06:14 -0600

    fix all broken tests from the join - job belongs to categories

--- spec/features/jobs/user_can_delete_a_job_spec.rb
+++ spec/features/jobs/user_can_delete_a_job_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 describe "When a user clicks the link to delete a job in job index page" do
   it "the user has deleted that job and now seees the company show/ job index page" do
     company = Company.create!(name: "ESPN")
-    company.jobs.create!(title: "Developer", level_of_interest: 90, city: "Denver")
-    company.jobs.create!(title: "Intern", level_of_interest: 99, city: "Denver")
+    category = Category.create(title: "Software")
+    company.jobs.create!(title: "Developer", level_of_interest: 90, city: "Denver", category: category)
+    company.jobs.create!(title: "Intern", level_of_interest: 99, city: "Denver", category: category)
 
     visit company_jobs_path(company)
     click_link "Delete", href: company_job_path(company, company.jobs.first)
@@ -18,8 +19,9 @@ end
 describe "When a user clicks the link to delete a job in job show page" do
   it "the user has deleted that job and now seees the company show/ job index page" do
     company = Company.create!(name: "ESPN")
-    company.jobs.create!(title: "Developer", level_of_interest: 90, city: "Denver")
-    company.jobs.create!(title: "Intern", level_of_interest: 99, city: "Denver")
+    category = Category.create(title: "Software")
+    company.jobs.create!(title: "Developer", level_of_interest: 90, city: "Denver", category: category)
+    company.jobs.create!(title: "Intern", level_of_interest: 99, city: "Denver",  category: category)
 
     visit company_job_path(company,company.jobs.first)
     click_link "Delete"
--- spec/features/jobs/user_can_edit_job_information_spec.rb
+++ spec/features/jobs/user_can_edit_job_information_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 describe "When a user visits the job edit page, changes the job information, and clicks 'Update Job'" do
   it "the user has updated the job and sees its show page" do
     company = Company.create!(name: "ESPN")
-    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    category = Category.create(title: "Software")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", category: category)
 
     visit edit_company_job_path(company,job)
 
@@ -18,7 +19,8 @@ end
 describe "When a user visits the the job show page" do
   it "the user can click on a link to take them to the job's edit page" do
     company = Company.create!(name: "ESPN")
-    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    category = Category.create(title: "Software")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", category: category)
 
     visit company_job_path(company, job)
     click_on "Edit"
@@ -30,7 +32,8 @@ end
 describe "When a user visits the job index page" do
   it "the user can click on a link to take them to a job's edit page" do
     company = Company.create!(name: "ESPN")
-    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    category = Category.create(title: "Software")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", category: category)
 
     visit company_jobs_path(company)
     click_on "Edit"
