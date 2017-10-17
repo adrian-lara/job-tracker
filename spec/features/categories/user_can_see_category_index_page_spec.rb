a4266978f734544843c90db17d8eae7c17850fcb
Adrian Lara <adrianblara@gmail.com>
2017-10-17 12:16:34 -0600

    category index feature test written

---
+++ spec/features/categories/user_can_see_category_index_page_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "When a user visits the category index page" do
+  it "the user sees a list of categories along with edit and delete links for each category" do
+    category_1 = Category.create(title: "Web Development")
+    category_2 = Category.create(title: "Back-End")
+
+    visit categories_path
+
+    expect(page).to have_content(category_1.title)
+    expect(page).to have_content(category_2.title)
+    expect(page).to have_link("Edit", href: edit_category_path(category_1))
+    expect(page).to have_link("Edit", href: edit_category_path(category_2))
+    expect(page).to have_link("Delete", href: categories_path(category_1))
+    expect(page).to have_link("Delete", href: categories_path(category_2))
+  end
+end
