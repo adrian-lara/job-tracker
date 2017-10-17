71f966935d1561e57a9f7ac3063a4e4b530163ca
Adrian Lara <adrianblara@gmail.com>
2017-10-17 16:12:37 -0600

    created new test for job show page. realized seed file wasn't creating/assigning categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,18 @@ Job.destroy_all
 COMPANIES = ["ESPN", "Aetna", "United Airlines", "Denver Public Schools", "Shopify", "Starbucks", "Pivotal Labs", "Captain U"]
 JOBS = ["Engineering", "Development", "Dev Ops", "Quality Assurance", "Teacher", "Product Manager", "Consultant", "Community Manager"]
 CITIES = ["Seattle", "Denver", "Portland", "Indianapolis", "Madison", "Orlando", "San Diego", "Austin", "Las Vegas", "Little Rock", "Boise", "Eugene", "Oakland"]
+CATEGORIES = ["Management", "Code Monkey", "Contractor"]
+CATEGORY_IDS = [1, 2, 3]
+
+CATEGORIES.each do |title|
+  Category.create(title: title)
+end
 
 COMPANIES.each do |name|
   company = Company.create!(name: name)
   puts "Created #{company.name}"
   10.times do |num|
-    company.jobs.create!(title: JOBS.sample, description: "What a great position!", level_of_interest: num + rand(100), city: CITIES.sample)
+    company.jobs.create!(title: JOBS.sample, description: "What a great position!", level_of_interest: num + rand(100), city: CITIES.sample, category_id: CATEGORY_IDS.sample)
     puts "  Created #{company.jobs[num].title}"
   end
 end
--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -13,3 +13,18 @@ describe "User sees a specific job" do
     expect(page).to have_content("70")
   end
 end
+
+describe "When a vists a company's show page, clicks on a link to a job page" do
+  it "the user sees the job show page" do
+    company = Company.create!(name: "ESPN")
+    category = Category.create(title: "Software")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", category: category)
+
+    visit company_path(company)
+    click_link "Developer"
+
+    expect(page).to have_content("ESPN")
+    expect(page).to have_content("Developer")
+    expect(page).to have_content("70")
+  end
+end
