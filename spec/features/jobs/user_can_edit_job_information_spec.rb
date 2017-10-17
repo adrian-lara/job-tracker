ba60a5303670239597090133a640533110efabeb
Adrian Lara <adrianblara@gmail.com>
2017-10-16 21:25:43 -0600

    user can get to edit page from job show page

--- spec/features/jobs/user_can_edit_job_information_spec.rb
+++ spec/features/jobs/user_can_edit_job_information_spec.rb
@@ -6,11 +6,23 @@ describe "When a user visits the job edit page, changes the job information, and
     job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
 
     visit edit_company_job_path(company,job)
-    
+
     fill_in "job[title]", with: "Intern"
     click_on "Update Job"
 
     expect(current_path).to eq(company_job_path(company, job))
     expect(page).to have_content("Intern")
+  end
+end
+
+describe "When a user visits the the job show page" do
+  it "the user can click on a link to take them to the job's edit page" do
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+
+    visit company_job_path(company, job)
+    click_on "Edit"
+
+    expect(current_path).to eq(edit_company_job_path(company,job))
   end
 end
