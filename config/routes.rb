eba3ae51ae20210b688bce50aaecbde38bd6ab33
Adrian Lara <adrianblara@gmail.com>
2017-10-17 12:25:23 -0600

    user can visit category index page and see edit and delete links

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resources :jobs
   end
 
-  resources :categories, only: [:index, :show, :new, :create]
+  resources :categories, only: [:index, :show, :new, :create, :edit]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/features/categories/user_can_see_category_index_page_spec.rb
+++ spec/features/categories/user_can_see_category_index_page_spec.rb
@@ -7,11 +7,13 @@ describe "When a user visits the category index page" do
 
     visit categories_path
 
+    save_and_open_page
+
     expect(page).to have_content(category_1.title)
     expect(page).to have_content(category_2.title)
     expect(page).to have_link("Edit", href: edit_category_path(category_1))
     expect(page).to have_link("Edit", href: edit_category_path(category_2))
-    expect(page).to have_link("Delete", href: categories_path(category_1))
-    expect(page).to have_link("Delete", href: categories_path(category_2))
+    expect(page).to have_link("Delete", href: category_path(category_1))
+    expect(page).to have_link("Delete", href: category_path(category_2))
   end
 end
