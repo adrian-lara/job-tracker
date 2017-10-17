b66c9e1cc1199c58ba582bcd427db2404c7e41f7
Adrian Lara <adrianblara@gmail.com>
2017-10-17 15:11:09 -0600

    user creates new job with drop down menu of all categories

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -11,6 +11,7 @@ class JobsController < ApplicationController
   end
 
   def create
+    binding.pry
     @job = @company.jobs.new(job_params)
     if @job.save
       flash[:success] = "You created #{@job.title} at #{@company.name}"
@@ -43,7 +44,7 @@ class JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description, :level_of_interest, :city)
+    params.require(:job).permit(:title, :description, :level_of_interest, :city, :category_id)
   end
 
   def set_company
