36cf158a28622b3206b0b535e2c14403fd1cf70a
Adrian Lara <adrianblara@gmail.com>
2017-10-17 17:50:09 -0600

    new feature test to create comment. job model has_many comments, job comment route has :create, comment controller has #create

---
+++ spec/features/comments/user_can_create_a_new_comment_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "When a user visits a job show page, enters content in comment field, clicks Create Comment" do
+  it "the user has created a new comment and is shown the same job show page with that new comment" do
+    company = Company.create!(name: "ESPN")
+    category = Category.create(title: "Software")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", category: category)
+
+    visit company_path(company)
+    click_link "Developer"
+    fill_in "comment[content]", with: "I'm leaving a comment here as a test."
+    click_on "Create Comment"
+
+    save_and_open_page
+    expect(job.comments.count).to eq(1)
+    expect(current_path).to eq(company_job_path(company, job))
+    expect(page).to have_content("I'm leaving a comment here as a test.")
+    expect(page).to have_content(job.comments.first.created_at)
+  end
+end
