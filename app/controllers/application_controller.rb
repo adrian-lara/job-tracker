b07a4e2ec1ea145aa2439a733c013e7603f3f223
Adrian Lara <adrianblara@gmail.com>
2017-10-19 00:59:21 -0600

    job count by interest level info available

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def dashboard
+    @jobs = Job.all
     render :'dashboard/index'
   end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -13,4 +13,16 @@ class Job < ApplicationRecord
     order(level_of_interest: :desc) if filter == "interest"
   end
 
+  def self.interest_counts
+    interest_level_data = []
+    interest_level_counts.each_pair do |k, v|
+      interest_level_data << {level: k, job_count: v}
+    end
+    interest_level_data
+  end
+
+  def self.interest_level_counts
+    order(level_of_interest: :desc).group(:level_of_interest).count
+  end
+
 end
